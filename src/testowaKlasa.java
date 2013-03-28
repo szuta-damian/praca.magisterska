c8f574fda35422a526d59f922030488659e291c8
szuta-damian <damianate@tlen.pl>
2013-03-28 20:13:50 +0100

    13

--- src/testowaKlasa.java
+++ src/testowaKlasa.java
@@ -6,7 +6,7 @@ public class testowaKlasa {
 	 */
 	public void metoda()
 	{
-		System.out.print("walka z gitem!");
+		System.out.print("walka z gitem! ");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
