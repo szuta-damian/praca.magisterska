3bcb8d6938ac4f6816a849552d3ca4f987c43a58
szuta-damian <damianate@tlen.pl>
2013-03-28 19:57:27 +0100

    walka z gitem

--- src/testowaKlasa.java
+++ src/testowaKlasa.java
@@ -4,6 +4,10 @@ public class testowaKlasa {
 	/**
 	 * @param args
 	 */
+	public void metoda()
+	{
+		System.out.print("walka z gitem");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
